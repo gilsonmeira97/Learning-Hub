5e8820394ddea783d0e5a923c2f92929c53c6f95
Gilson Meira <gilsonmeira97@gmail.com>
2025-03-05 01:15:16 +0100

    progress java course¨

--- JavaJUnit5/src/test/java/com/testing/learning/StringFuncsTest.java
+++ JavaJUnit5/src/test/java/com/testing/learning/StringFuncsTest.java
@@ -13,6 +13,12 @@ class StringFuncsTest {
     }
 
     @Test
+    void reverseString_NotExpected() { assertNotEquals("Java", rs.reverseString("Java"), "Test Fail!"); }
+
+    @Test
+    void reverseString_BooleanTest() { assertTrue(rs.reverseString("Dia").equals("aiD")); }
+
+    @Test
     void reverseString_MultipleWord() {
         assertEquals("ysae si avaJ", rs.reverseString("Java is easy"));
     }
