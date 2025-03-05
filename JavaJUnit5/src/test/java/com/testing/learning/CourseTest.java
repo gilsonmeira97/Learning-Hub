514fbe55c1b634792024ae999998018347819113
Gilson Meira de Mendonca <gilsonmeira97@gmail.com>
2025-03-05 18:05:18 +0100

    progress Junit5

--- JavaJUnit5/src/test/java/com/testing/learning/CourseTest.java
+++ JavaJUnit5/src/test/java/com/testing/learning/CourseTest.java
@@ -1,15 +1,22 @@
 package com.testing.learning;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CourseTest {
 
+    JavaCourse  jc;
+
+    @BeforeEach
+    void init() {
+        jc = new JavaCourse();
+    }
+
     @Test
     void isInstanceOf() {
-        assertInstanceOf(Course.class, new JavaCourse());
-        assert
+        assertInstanceOf(Course.class, jc);
     }
 
 }
