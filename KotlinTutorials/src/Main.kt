83fcda525b89bb3f3c56ed52b6fcae42d12c8e27
Gilson Meira de Mendonca <gilsonmeira97@gmail.com>
2025-03-05 20:03:19 +0100

    progress on Kotlin/Android

--- KotlinTutorials/src/Main.kt
+++ KotlinTutorials/src/Main.kt
@@ -1,3 +1,33 @@
 fun main() {
-    println("Hello World!")
+    println("Hello World!");
+    calc(3, 6);
+
+    println(getMax(1,5,7,9,6,11,154,567,98));
+
+    val testPrime = 12;
+    println("The number $testPrime is Prime? ${testPrime.isPrime()}")
+}
+
+fun calc(a: Int, b: Int) {
+    println(a + b);
+}
+
+fun getMax(vararg numbers: Int = intArrayOf(0)): Int {
+    var max = numbers[0];
+    for(number in numbers) {
+        if(number > max) {
+            max = number;
+        }
+    }
+    return max;
+}
+
+fun Int.isPrime(): Boolean {
+    for(n in 2 until this - 1) {
+        if(this % n == 0) {
+            return false;
+        }
+    }
+
+    return true;
 }
