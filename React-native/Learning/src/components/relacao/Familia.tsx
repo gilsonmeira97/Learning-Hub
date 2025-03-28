b495d7759e31f50b00f572f1ac28bf1a6908ca79
Gilson Meira de Mendonca <gilsonmeira97@gmail.com>
2025-03-28 15:27:08 +0100

    progress on course

--- React-native/Learning/src/components/relacao/Familia.tsx
+++ React-native/Learning/src/components/relacao/Familia.tsx
@@ -2,8 +2,8 @@ import React, { ReactNode } from 'react';
 import { Text } from 'react-native';
 import Estilo from '../estilo';
 
-export default ({children}: {children: ReactNode[]}) => {
+export default ({children}: {children: ReactNode}) => {
     return (
-        children[2]
+        children
     )
 }
