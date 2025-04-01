3b3036619fb2a6ac8245be538fe8f61e84ee6e25
Gilson Meira de Mendonca <gilsonmeira97@gmail.com>
2025-04-01 14:25:12 +0200

    new project spring boot

---
+++ SpringCourse/spring-ecommerce/src/main/java/com/learning/spring_ecommerce/controller/ProductController.java
@@ -0,0 +1,37 @@
+package com.learning.spring_ecommerce.controller;
+
+import com.learning.spring_ecommerce.model.Product;
+import com.learning.spring_ecommerce.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+
+    @Autowired
+    ProductService service;
+
+        @GetMapping("/products")
+        public ResponseEntity<List<Product>> getProducts() {
+            return new ResponseEntity<>(service.getAllProducts(), HttpStatus.OK);
+        }
+
+    @GetMapping("/product/{productName}")
+    public ResponseEntity<Product> getProductByName(@PathVariable String productName){
+        Optional<Product> result = service.getByName(productName);
+        if(result.isPresent())
+            return new ResponseEntity<>(result.get(), HttpStatus.OK);
+        else
+            return new ResponseEntity<>(new Product(), HttpStatus.BAD_REQUEST);
+    }
+
+}
---
+++ SpringCourse/spring-ecommerce/src/main/java/com/learning/spring_ecommerce/service/ProductService.java
@@ -0,0 +1,23 @@
+package com.learning.spring_ecommerce.service;
+
+import com.learning.spring_ecommerce.model.Product;
+import com.learning.spring_ecommerce.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    @Autowired
+    ProductRepository repo;
+
+    public List<Product> getAllProducts() {
+        return repo.findAll();
+    }
+
+    public Optional<Product> getByName(String name) {
+        return Optional.ofNullable(repo.findByName(name));
+    }
+}
